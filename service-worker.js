9e278bf125e1586b4ae9cb62486cd01d2d1c15ee
starcity2cwm <starcity2.cwm@gmail.com>
2025-12-25 15:44:02 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
  * SERVICE WORKER - Enables offline functionality
  */
 
-const CACHE_NAME = 'starcity-shop-v2';
+const CACHE_NAME = 'starcity-shop-v3';
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
